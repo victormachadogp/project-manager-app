5d8080626b1714ef2bb95f1dc7e7efdad8693864
Victor Machado <victormachadogp@gmail.com>
2025-07-17 18:15:02 -0300

    feat: Project card component

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <TheHeader />
+
+  <ProjectCard />
 </template>
 
 <script setup lang="ts">
 import TheHeader from '@/components/TheHeader.vue'
+import ProjectCard from '@/components/ProjectCard.vue'
 </script>
