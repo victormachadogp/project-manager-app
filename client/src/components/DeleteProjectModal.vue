9a448212077f41a257e5c9767696c31ad64f27ec
Victor Machado <victormachadogp@gmail.com>
2025-07-17 19:43:02 -0300

    feat: Add DeleteProjectModal component and IconTrash for project deletion

---
+++ client/src/components/DeleteProjectModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <ModalBase
+    title="Remover projeto"
+    :icon="IconTrash"
+    @confirm="$emit('confirm')"
+    @cancel="$emit('cancel')"
+  >
+    <p class="text-base text-center leading-relaxed text-gray-500">
+      Essa ação removerá definitivamente o projeto:
+    </p>
+    <h3 class="text-2xl text-[#1C1930] text-center">{{ project?.name }}</h3>
+  </ModalBase>
+</template>
+
+<script setup lang="ts">
+import ModalBase from './ModalBase.vue'
+import IconTrash from './icons/IconTrash.vue'
+import type { Project } from '../types/project'
+
+defineProps<{
+  project: Project | null
+}>()
+
+defineEmits<{
+  (e: 'cancel'): void
+  (e: 'confirm'): void
+}>()
+</script>
