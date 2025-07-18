6b153dc3b37826c2e8a9b3b954bccd5290809a54
Victor Machado <victormachadogp@gmail.com>
2025-07-18 09:29:32 -0300

    style: Add max height to header container for improved layout

--- client/src/components/TheHeader.vue
+++ client/src/components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-[#1C1930] shadow-custom">
     <div class="max-w-[1860px] mx-auto">
-      <div class="flex justify-between items-center mx-5 sm:mx-12">
+      <div class="flex justify-between items-center mx-5 sm:mx-12 max-h-[80px]">
         <div class="opacity-0"></div>
         <div class="flex items-center gap-2 text-white">
           <img class="my-4 sm:my-2 w-20 sm:w-18" src="../assets/logo-symbol.svg" alt="" />
