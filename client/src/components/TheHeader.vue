58163ee272762d7f82fb18ed414725c0546e65cf
Victor Machado <victormachadogp@gmail.com>
2025-07-18 00:15:54 -0300

    feat: Add logo-symbol.svg and update TheHeader to use new logo

--- client/src/components/TheHeader.vue
+++ client/src/components/TheHeader.vue
@@ -3,7 +3,10 @@
     <div class="max-w-[1860px] mx-auto">
       <div class="flex justify-between items-center mx-5 sm:mx-12">
         <div class="opacity-0"></div>
-        <img class="my-4 sm:my-2 w-20 sm:w-28" src="../assets/logo.png" alt="" />
+        <div class="flex items-center gap-2 text-white">
+          <img class="my-4 sm:my-2 w-20 sm:w-18" src="../assets/logo-symbol.svg" alt="" />
+          <p class="text-lg w-2/5">Gerenciador de Projetos</p>
+        </div>
         <button>
           <IconSearch class="text-white" />
         </button>
