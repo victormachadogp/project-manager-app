919c83e8d01cfe7edad5e91c20a3b22ae5b60e7a
Victor Machado <victormachadogp@gmail.com>
2025-07-17 21:40:20 -0300

    feat: Update Tailwind CSS to use lastest documentation version

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,3 +5,13 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <RouterView />
 </template>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f4f2ff;
+}
+</style>
