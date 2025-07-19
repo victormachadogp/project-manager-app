791db60926a564eb47a5ca7caf9d8879de4c1c1e
Victor Machado <victormachadogp@gmail.com>
2025-07-18 21:45:26 -0300

    fix: Fix color in ProjectCard

--- client/src/components/ProjectCard.vue
+++ client/src/components/ProjectCard.vue
@@ -65,22 +65,29 @@
     <div class="p-5">
       <a href="#">
         <h5 class="mb-2 text-xl font-bold tracking-tight text-[#1F1283]">
-          <template v-for="part in getHighlightedPartsForCurrentSearch(project.name)" :key="part.text">
+          <template
+            v-for="part in getHighlightedPartsForCurrentSearch(project.name)"
+            :key="part.text"
+          >
             <span v-if="!part.isHighlighted">{{ part.text }}</span>
             <span v-else class="bg-yellow-200">{{ part.text }}</span>
           </template>
         </h5>
       </a>
       <span class="mb-3 text-[#717171] font-bold"
-        >Cliente: <span class="font-normal">
-          <template v-for="part in getHighlightedPartsForCurrentSearch(project.client)" :key="part.text">
+        >Cliente:
+        <span class="font-normal">
+          <template
+            v-for="part in getHighlightedPartsForCurrentSearch(project.client)"
+            :key="part.text"
+          >
             <span v-if="!part.isHighlighted">{{ part.text }}</span>
             <span v-else class="bg-yellow-200">{{ part.text }}</span>
           </template>
         </span></span
       >
     </div>
-    <div class="border-t border-[##ECECEC] mx-3 pt-2">
+    <div class="border-t border-[#ECECEC] mx-3 pt-2">
       <div class="p-2 flex items-center gap-4">
         <IconStartDate />
         <span class="text-[#717171] font-normal top-px relative">{{
