683a51dcaf465c81145760967e226c8ae5cdc2a1
Victor Machado <victormachadogp@gmail.com>
2025-07-19 15:06:12 -0300

    style: Update ModalBase component background opacity for improved visibility

--- client-v2/src/components/ModalBase.vue
+++ client-v2/src/components/ModalBase.vue
@@ -2,7 +2,7 @@
   <div
     id="default-modal"
     tabindex="-1"
-    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-50 flex justify-center"
+    class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black/80 bg-opacity-50 flex justify-center"
   >
     <div class="relative p-4 w-full max-w-md max-h-full">
       <div class="relative bg-white rounded-lg shadow flex flex-col items-center">
