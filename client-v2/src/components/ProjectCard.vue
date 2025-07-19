7d98b373fe8c10d08d99250b9d0e338ee5e5c5b2
Victor Machado <victormachadogp@gmail.com>
2025-07-18 22:43:14 -0300

    style: Update ProjectCard with new favorite icons and improve button styling

--- client-v2/src/components/ProjectCard.vue
+++ client-v2/src/components/ProjectCard.vue
@@ -47,61 +47,54 @@
       </div>
       <div class="flex items-center gap-2 self-start">
         <button @click="toggleFavorite">
-          <svg
-            width="22"
-            height="22"
-            viewBox="0 0 22 22"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              class="shadow"
-              d="M1.16398 9.1776C0.845199 8.88374 1.01836 8.35251 1.44953 8.30155L7.55901 7.5792C7.73474 7.55843 7.88738 7.44843 7.96149 7.28825L10.5384 1.71938C10.7203 1.32636 11.2808 1.32628 11.4626 1.7193L14.0395 7.28813C14.1136 7.44831 14.2653 7.55861 14.441 7.57938L20.5508 8.30155C20.982 8.35251 21.1547 8.8839 20.8359 9.17776L16.3195 13.3419C16.1896 13.4616 16.1317 13.6399 16.1662 13.8129L17.3649 19.828C17.4495 20.2525 16.9962 20.5815 16.6174 20.3701L11.249 17.3738C11.0946 17.2877 10.907 17.2881 10.7526 17.3742L5.3836 20.3693C5.00472 20.5807 4.55066 20.2525 4.63528 19.828L5.83411 13.8133C5.8686 13.6403 5.8109 13.4616 5.68098 13.3418L1.16398 9.1776Z"
-              :fill="project.isFavorite ? '#FFB23D' : 'none'"
-              :stroke="project.isFavorite ? 'none' : '#71717a'"
-              :stroke-width="project.isFavorite ? '1' : '2'"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </button>
-
-        <button
-          @click="showOptions = !showOptions"
-          class="bg-white w-8 h-8 rounded-full flex justify-center"
-          ref="dropdownRef"
-        >
-          <span class="text-[#0a0a0b] font-bold">...</span>
-
           <div
-            v-if="showOptions"
-            class="absolute right-0 top-0 mt-9 mb-2 bg-white rounded-lg shadow-lg py-2 min-w-[120px]"
+            class="inline-flex items-center justify-center gap-2 rounded-md h-8 w-8 p-0 transition-all duration-200 hover:scale-110 hover:bg-[#f4f4f5]"
           >
-            <RouterLink
-              :to="`/project/${project.id}`"
-              class="block px-4 py-2 text-left hover:bg-gray-100 text-sm text-[#695CCD]"
-            >
-              Editar
-            </RouterLink>
-            <button
-              @click="handleDelete"
-              class="block w-full text-left px-4 py-2 hover:bg-gray-100 text-sm text-[#695CCD] border-t border[#F4F2FF]"
-            >
-              Excluir
-            </button>
+            <IconStarFilled v-if="project.isFavorite" />
+            <IconStar v-else />
           </div>
         </button>
+
+        <div
+          class="inline-flex items-center justify-center gap-2 rounded-md transition-all duration-200 hover:bg-[#f4f4f5]"
+        >
+          <button
+            @click="showOptions = !showOptions"
+            class="w-8 h-8 rounded-full flex justify-center"
+            ref="dropdownRef"
+          >
+            <span class="text-[#0a0a0b] font-bold">...</span>
+
+            <div
+              v-if="showOptions"
+              class="absolute right-[28px] top-0 mt-14 mb-2 bg-white border border-[#e4e4e7] rounded-lg shadow-lg p-1 min-w-[120px]"
+            >
+              <RouterLink
+                :to="`/project/${project.id}`"
+                class="block px-4 py-2 text-left hover:bg-gray-100 text-sm rounded text-[#0a0a0b]"
+              >
+                Editar
+              </RouterLink>
+              <button
+                @click="handleDelete"
+                class="block w-full text-left px-4 py-2 hover:bg-gray-100 text-sm rounded text-[#ef4544]"
+              >
+                Excluir
+              </button>
+            </div>
+          </button>
+        </div>
       </div>
     </div>
     <div class="border-t border-[#f2f1f3] pt-2">
-      <div class="p-2 flex items-center gap-4">
-        <IconStartDate class="" />
+      <div class="p-2 flex items-center gap-2">
+        <IconStartDate class="relative top-px" />
         <span class="text-[#717171] text-sm font-normal top-px relative">{{
           formatDate(project.startDate)
         }}</span>
       </div>
-      <div class="p-2 flex items-center gap-4">
-        <IconEndDate />
+      <div class="p-2 flex items-center gap-2">
+        <IconEndDate class="relative bottom-px" />
         <span class="text-[#717171] text-sm font-normal top-px relative">{{
           formatDate(project.endDate)
         }}</span>
@@ -116,6 +109,8 @@ import { useProjectCard } from '../composables/useProjectCard'
 import { useHighlight } from '../composables/useHighlight'
 import IconStartDate from '@/components/icons/IconStartDate.vue'
 import IconEndDate from '@/components/icons/IconEndDate.vue'
+import IconStar from '@/components/icons/IconStar.vue'
+import IconStarFilled from '@/components/icons/IconStarFilled.vue'
 
 const props = defineProps<{ project: Project }>()
 const emit = defineEmits<{
